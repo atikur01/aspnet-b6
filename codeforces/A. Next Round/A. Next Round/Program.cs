901d9d7c92e638d8341b27df4279d4713eb455ca
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-11-15 19:56:01 +0600

    1

---
+++ codeforces/A. Next Round/A. Next Round/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A._Next_Round
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine().Split(' ');
+            var n = int.Parse(str[0]);
+            var k = int.Parse(str[1]);
+            var count = 0;
+
+            var input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (int.Parse(input[0]) > k) count++;
+            }
+            
+
+            Console.WriteLine(count);
+        }
+    }
+}
