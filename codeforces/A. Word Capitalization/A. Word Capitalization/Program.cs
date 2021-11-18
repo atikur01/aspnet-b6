c167df8ba982c7a84938c9136b956f35ddf33137
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-11-18 08:38:51 +0600

    1

---
+++ codeforces/A. Word Capitalization/A. Word Capitalization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A._Word_Capitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ( i==0 && Char.IsUpper(str[i]) == false)
+                {
+                    Console.Write(Char.ToUpper(str[i]));
+                    continue;
+                }
+                Console.Write(str[i]);
+            }
+           
+                
+            
+        }
+    }
+}
