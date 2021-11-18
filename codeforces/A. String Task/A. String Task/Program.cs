f9e7e2bcf860e5c3d70344b5bc3f84ed834ba8b1
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-11-18 08:24:47 +0600

    1

---
+++ codeforces/A. String Task/A. String Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A._String_Task
+{
+    class Program
+    {
+        public static bool CheckVowel(char ch)
+        {
+            if(ch=='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'|| ch=='y')
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                if ( CheckVowel(str[i]) ){
+                    continue;
+                }
+                if (CheckVowel(str[i]) == false)
+                {
+                    Console.Write(".");
+                    Console.Write(str[i]);
+                }
+            }
+        }
+    }
+}
