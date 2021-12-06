213b5209be0e92a9c83d676d1424d15340aaa476
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-12-06 15:56:10 +0600

    1

---
+++ Assignment 1/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Session
+    {
+        public int DurationInHour { get; set; }
+        public string LearningObjective { get; set; }
+
+        public Session()
+        {
+            DurationInHour = 3;
+            LearningObjective = "describe the knowledge(s), skills, values, and habits of mind";
+        }
+    }
+}
