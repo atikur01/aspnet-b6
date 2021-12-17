51acc817edd8e228531d7f97ae0b859b0891436a
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-12-17 10:49:54 +0600

    1

---
+++ A. Bit++/A. Bit++/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace A._Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            int x = 0;
+
+            while (n > 0)
+            {
+
+                var str = Console.ReadLine();
+
+                foreach (var item in str)
+                {
+                    if (item == '+')
+                    {
+                        x += 1;
+                        break;
+                    }
+
+                    if (item == '-')
+                    {
+                        x -= 1;
+                        break;
+                    }
+                }
+
+                n--;
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ A. Stones on the Table/A. Stones on the Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A._Stones_on_the_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var s = Console.ReadLine();
+
+            int count = 0;
+
+            for (int i = 0; i <= s.Length - 2; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    count++;
+                }
+
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
