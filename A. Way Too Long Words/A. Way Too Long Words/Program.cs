ad8dd8fe485a0dc274134f8b433fd7f2582db7f8
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2021-12-17 10:42:17 +0600

    1

---
+++ A. Way Too Long Words/A. Way Too Long Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A._Way_Too_Long_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine();
+
+                if (str.Length > 10)
+                {
+                    Console.WriteLine($"{str[0]}{str.Length - 2}{str[str.Length - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
