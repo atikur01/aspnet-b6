7f842ad751e2e364836d23629cbf5bbfb8a82599
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2022-05-30 23:13:26 +0600

    added SQS Operation codes

---
+++ SQS Operation/SQS Operation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SQS_Operation
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            QueueOperation queueOperation = new QueueOperation();
+
+            var url = "https://sqs.us-east-1.amazonaws.com/667432834983/AtiksQueue";
+            var body = "hello world";
+      
+            await queueOperation.SendMessage(url,body);
+
+            var receiveMessageResponse = await queueOperation.GetMessage(url);
+   
+            await queueOperation.DeleteReadMessages(url, receiveMessageResponse);
+           
+        }
+    }
+}
---
+++ SQS Operation/SQS Operation/QueueOperation.cs
@@ -0,0 +1,59 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS_Operation
+{
+    public class QueueOperation
+    {
+        private static IAmazonSQS sqsClient;
+
+        public QueueOperation()
+        {
+            sqsClient = new AmazonSQSClient(RegionEndpoint.USEast1);
+        }
+
+        public async Task SendMessage(string qUrl, string messageBody)
+        {  
+            SendMessageResponse responseSendMsg =
+              await sqsClient.SendMessageAsync(qUrl, messageBody);
+            Console.WriteLine($"Message added to queue\n  {qUrl}");
+            Console.WriteLine($"HttpStatusCode: {responseSendMsg.HttpStatusCode}");
+        }
+
+        public async Task<ReceiveMessageResponse> GetMessage(string qUrl)
+        {
+            var x = await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = qUrl,
+                MaxNumberOfMessages = 10,
+                WaitTimeSeconds = 20
+            });
+
+            //Console.WriteLine($"Count : {x.Messages.Count}");
+            foreach (var item in x.Messages)
+            {
+
+                Console.WriteLine(item.Body);
+            }
+            return x;   
+        }
+
+        public async Task<ReceiveMessageResponse> DeleteReadMessages(string queueUrl, ReceiveMessageResponse receiveMessageResponse)
+        {
+            for (int i = 0; i < receiveMessageResponse.Messages.Count; i++)
+            {
+                var deleteMessageRequest = new DeleteMessageRequest
+                {
+                    QueueUrl = queueUrl,
+                    ReceiptHandle = receiveMessageResponse.Messages[i].ReceiptHandle,
+                };
+
+                  await sqsClient.DeleteMessageAsync(deleteMessageRequest);
+            }
+           
+            return receiveMessageResponse;
+        }
+
+    }
+}
