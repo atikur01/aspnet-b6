ffba6e77fa299e8656306998de2e06d1a2d8fde3
MD. ATIKUR RAHMAN <coc13259@gmail.com>
2022-05-31 22:41:08 +0600

    added S3 Bucket Operation

---
+++ S3 Bucket Operation/S3 Bucket Operation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace S3_Bucket_Operation
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            S3Operation s3Operation = new S3Operation();
+            string LOCAL_PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string OBJECT_NAME1 = "objectname1.txt";
+            var path = $"{LOCAL_PATH}\\{OBJECT_NAME1}";
+
+            //await s3Operation.UploadObjectFromFileAsync("storageudemy", "objectname1.txt", path);
+
+            //await s3Operation.ReadObjectDataAsync("storageudemy", "objectname1.txt");
+
+            //await s3Operation.DeleteObjectNonVersionedBucketAsync("storageudemy", "objectname1.txt");
+
+        }
+    }
+}
---
+++ S3 Bucket Operation/S3 Bucket Operation/S3Operation.cs
@@ -0,0 +1,110 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3_Bucket_Operation
+{
+    public class S3Operation
+    {
+        private IAmazonS3? _client { get; set; }
+
+        public S3Operation()
+        {
+            _client = new AmazonS3Client(RegionEndpoint.USEast1);
+        }
+
+        public async Task UploadObjectFromFileAsync(
+            string bucketName,
+            string objectName,
+            string filePath)
+        {
+            IAmazonS3 client = _client;
+            try
+            {
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = objectName,
+                    FilePath = filePath,
+                    ContentType = "text/plain",
+                };
+
+                putRequest.Metadata.Add("x-amz-meta-title", "someTitle");
+
+                PutObjectResponse response = await client.PutObjectAsync(putRequest);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        public async Task ReadObjectDataAsync(string bucketName,string keyName)
+        {
+            string responseBody = "";
+            IAmazonS3 client = _client;
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                using (GetObjectResponse response = await client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
+                    string contentType = response.Headers["Content-Type"];
+                    Console.WriteLine("Object metadata, Title: {0}", title);
+                    Console.WriteLine("Content type: {0}", contentType);
+
+                    responseBody = reader.ReadToEnd().ToString(); // Now you process the response body.
+
+                    await File.WriteAllTextAsync("WriteText.txt", responseBody);
+
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                // If bucket or object does not exist
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading object", e.Message);
+            }
+        }
+
+        public async Task DeleteObjectNonVersionedBucketAsync(string bucketName,string keyName)
+        {
+            IAmazonS3 client = _client;
+            try
+            {
+                var deleteObjectRequest = new DeleteObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                Console.WriteLine("Deleting an object");
+                await client.DeleteObjectAsync(deleteObjectRequest);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when deleting an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when deleting an object", e.Message);
+            }
+        }
+    }
+
+}
+
